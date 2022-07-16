ebd011e8a242259fd72b62b3bc9b46798da469b6
gongchangyou <gongchangyou@gmail.com>
2022-07-16 16:22:21 +0800

    add method

--- src/main/java/com/mouse/feign_consumer/controller/TestController.java
+++ src/main/java/com/mouse/feign_consumer/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.mouse.feign_consumer.controller;
 
+import com.mouse.feigndemo.intf.Param;
+import com.mouse.feigndemo.intf.Result;
 import com.mouse.feigndemo.intf.StuRpcService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author gongchangyou
@@ -18,9 +22,21 @@ public class TestController {
     @Autowired
     private StuRpcService stuRpcService;
 
-    @RequestMapping("feign")
+    @RequestMapping("add")
     int add(int a , int b) {
         return stuRpcService.add(a, b);
+    }
+
+    @RequestMapping("stringTest")
+    String stringTest(Param param) {
+        log.info("param {}", param);
+        return stuRpcService.stringTest(param);
+    }
+
+    @RequestMapping("resultTest")
+    Result<List<Long>> resultTest(Param param) {
+        log.info("param {}", param);
+        return stuRpcService.resultTest(param);
     }
 
 }
